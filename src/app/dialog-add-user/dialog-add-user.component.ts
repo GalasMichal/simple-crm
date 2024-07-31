2399ab426a15031d6938738cbc8f3f1026172ac6
Michal Galas <michalgalas1989@gmail.com>
2024-08-01 01:09:44 +0200

    added first object to firebase with function, test , styling dialog window and functionality, by user component created first table

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, MatNativeDateModule, NativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
+import { collection, Firestore } from '@angular/fire/firestore';
+import { doc, setDoc } from "firebase/firestore";
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -19,6 +22,8 @@ import { User } from '../../models/user.class';
     FormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatProgressBarModule,
+
   ],
   providers: [
     { provide: DateAdapter, useClass: NativeDateAdapter },
@@ -28,12 +33,29 @@ import { User } from '../../models/user.class';
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
+  firestore: Firestore = inject(Firestore);
+  dialogRef: MatDialogRef<DialogAddUserComponent> = inject(MatDialogRef);
   user = new User();
   birthDate!: Date;
+  loading = false;
 
-  saveUser(){
+ async saveUser(){
     this.user.birthDate = this.birthDate.getTime();
-    console.log('Current user is', this.user)
+    console.log('Current user is', this.user);
+   this.loading = true;
+   const newUserRef = doc(collection(this.firestore, "users"))
+   try {
+    await setDoc(newUserRef, this.user.toJSON());
+    console.log('Adding user complete');
+    this.loading = false;
+    this.dialogRef.close();
+  } catch (error) {
+    console.error('Error adding user: ', error);
+    this.loading = false;
   }
+}
 
+closeDialog() {
+  this.dialogRef.close();
+}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,12 +6,12 @@ import { FormControl } from '@angular/forms';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule],
+  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
@@ -22,11 +22,9 @@ export class UserComponent {
 
   user = new User();
 
-  constructor(){
-   
-  }
+  constructor() {}
 
   openDialog(): void {
-    this.dialog.open(DialogAddUserComponent)
+    this.dialog.open(DialogAddUserComponent);
   }
 }
