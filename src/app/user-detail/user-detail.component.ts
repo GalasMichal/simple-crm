99cbdb6ac6b3b4d64e66ddcdc7ed2c62deec6567
Michal Galas <michalgalas1989@gmail.com>
2024-08-02 14:30:08 +0200

    get ID from route link and save it in variable

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute, } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,4 +9,18 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss',
 })
-export class UserDetailComponent {}
+export class UserDetailComponent {
+  userId: string | null = null;
+
+  constructor(private route:ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+      this.route.paramMap.subscribe( paramMap => {
+        this.userId = paramMap.get('id');
+        console.log('user id ist', this.userId)
+    })
+
+  }
+}
